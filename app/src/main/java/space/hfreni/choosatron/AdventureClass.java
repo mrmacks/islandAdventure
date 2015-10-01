4d2ee2fe8bdee2a3330d08c3e2e0bf6733798a16
Harrison Freni <harrison@harrisonfreni.me>
2015-09-30 19:10:16 -0700

    Class Structure Set Up

--- app/src/main/java/space/hfreni/choosatron/AdventureClass.java
+++ app/src/main/java/space/hfreni/choosatron/AdventureClass.java
@@ -4,5 +4,55 @@ package space.hfreni.choosatron;
  * Created by hfreni on 30/09/2015.
  */
 public class AdventureClass {
-    
+
+    // The Declarations and Stuff
+    public String   sNameOfAdv;
+    public String   sStoryBody;
+    public int      iNumberOfChoices;
+    public boolean  bIsPlayerDead;
+
+    //Default Constructor
+    public AdventureClass() {
+
+    }
+
+    // Constructor, cuz u gotta construct that shit.
+    // TFW I could Have used this.
+    public AdventureClass(String adventureName, String story, int iNumOfChoices) {
+        sNameOfAdv = adventureName;
+        sStoryBody = story;
+        iNumberOfChoices = iNumOfChoices;
+    }
+
+    public String getsNameOfAdv() {
+        return sNameOfAdv;
+    }
+
+    public void setsNameOfAdv(String sNameOfAdv) {
+        this.sNameOfAdv = sNameOfAdv;
+    }
+
+    public String getsStoryBody() {
+        return sStoryBody;
+    }
+
+    public void setsStoryBody(String sStoryBody) {
+        this.sStoryBody = sStoryBody;
+    }
+
+    public int getiNumberOfChoices() {
+        return iNumberOfChoices;
+    }
+
+    public void setiNumberOfChoices(int iNumberOfChoices) {
+        this.iNumberOfChoices = iNumberOfChoices;
+    }
+
+    public boolean isPlayerDead() {
+        return bIsPlayerDead;
+    }
+
+    public void setPlayerDead(boolean bIsPlayerDead) {
+        this.bIsPlayerDead = bIsPlayerDead;
+    }
 }
